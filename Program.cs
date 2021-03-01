524a43c7f8662914916c297adc51ca1772b1b050
NoobHereo <NoobHereo@users.noreply.github.com>
2021-03-01 19:17:58 +0100

    Project Base

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MapExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Experimental JSON Map Exporter";
+
+            Console.WriteLine("Map Exporter Status [READY]");
+
+            
+        }
+    }
+}
